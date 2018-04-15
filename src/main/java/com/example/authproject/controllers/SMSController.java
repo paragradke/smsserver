52b8e2c16722f88b1c0d2304a5fd623ce1d32257
Parag Radke <parag@example.com>
2018-04-15 18:07:47 +0530

    Fix 200 ok response

--- src/main/java/com/example/authproject/controllers/SMSController.java
+++ src/main/java/com/example/authproject/controllers/SMSController.java
@@ -5,6 +5,7 @@ import com.example.authproject.context.Context;
 import com.example.authproject.context.LocalContext;
 import com.example.authproject.mappers.models.Account;
 import com.example.authproject.requests.SMSRequest;
+import com.example.authproject.responses.Response;
 import com.example.authproject.services.AccountService;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
@@ -12,6 +13,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 
 import javax.validation.Valid;
+import java.util.ArrayList;
 
 
 @RestController
@@ -34,22 +36,24 @@ public class SMSController {
 
 
     @RequestMapping(value="/inbound/sms",method = RequestMethod.POST)
-    public Account inboundSMS(@Valid @RequestBody SMSRequest smsRequest) {
+    public Response inboundSMS(@Valid @RequestBody SMSRequest smsRequest) {
         Context context = LocalContext.get();
         Account account = context.getAccount();
         logger.info("inboundSMS request received for account name:"+ account.getUsername()
                 + " to:" + smsRequest.getTo() + " from: "+ smsRequest.getFrom()+ " text: "+ smsRequest.getText());
-        logger.info("Response : "+ account);
-        return account;
+        Response response = new Response("inbound sms ok", new ArrayList<>());
+        logger.info("Response : "+ response);
+        return response;
     }
 
     @RequestMapping(value="/outbound/sms",method = RequestMethod.POST)
-    public Account outboundSMS(@Valid @RequestBody SMSRequest smsRequest) {
+    public Response outboundSMS(@Valid @RequestBody SMSRequest smsRequest) {
         Context context = LocalContext.get();
         Account account = context.getAccount();
         logger.info("outboundSMS request received for account name:"+ account.getUsername()
                 + " to:" + smsRequest.getTo() + " from: "+ smsRequest.getFrom()+ " text: "+ smsRequest.getText());
-        logger.info("Response : "+ account);
-        return account;
+        Response response = new Response("outbound sms ok", new ArrayList<>());
+        logger.info("Response : "+ response);
+        return response;
     }
 }
