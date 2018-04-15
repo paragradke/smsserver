784e0c2cdfd16d7336ce312637d96f0b36edc07b
Parag Radke <parag@example.com>
2018-04-15 17:57:28 +0530

    Implement error handling for unknown exceptions

--- src/main/java/com/example/authproject/advices/ExceptionHandlerAdvice.java
+++ src/main/java/com/example/authproject/advices/ExceptionHandlerAdvice.java
@@ -10,6 +10,9 @@ import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
 
+import java.util.ArrayList;
+import java.util.List;
+
 @RestControllerAdvice
 public class ExceptionHandlerAdvice {
 
@@ -18,5 +21,14 @@ public class ExceptionHandlerAdvice {
         BindingResult result = ex.getBindingResult();
         Response response = new Response("", ValidationUtil.fromBindingErrors(result));
         return new ResponseEntity<Response>(response, HttpStatus.BAD_REQUEST);
+    }
+
+    @ExceptionHandler(Exception.class)
+    public ResponseEntity<Response> unknownException(MethodArgumentNotValidException ex) {
+        BindingResult result = ex.getBindingResult();
+        List<String> errors = new ArrayList<>();
+        errors.add("unknown failure");
+        Response response = new Response("", errors);
+        return new ResponseEntity<Response>(response, HttpStatus.INTERNAL_SERVER_ERROR);
     }
 }
